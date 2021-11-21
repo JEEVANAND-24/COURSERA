b6300aac2c52269fa412bef0cc7b56f95171dd5d
JEEVANAND R <77654339+JEEVANAND-24@users.noreply.github.com>
2021-11-21 10:12:19 +0530

    Update js-week4-1.js

--- js-week4-1.js
+++ js-week4-1.js
@@ -3,9 +3,9 @@
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
-            byeSpeaker.speak(names[i]);
+            byeSpeaker.speaker(names[i]);
         } else {
-            helloSpeaker.speak(names[i]);
+            helloSpeaker.speaker(names[i]);
         }
     }
 })();
